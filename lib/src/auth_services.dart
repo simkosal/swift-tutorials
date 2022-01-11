88899beea70d79b90019f13db28c4e48b0c3c3ca
kosal.sim <kosal.sim@ecoinsoft.com>
2022-01-11 14:50:29 +0700

    Remove header deviceId from request pin signIn

--- lib/src/auth_services.dart
+++ lib/src/auth_services.dart
@@ -30,7 +30,7 @@ class AuthServices {
       "phoneNumber": this.phoneNumber,
     };
     final header = {
-      "deviceId": await this.deviceInfoService.getDeviceId(),
+      // "deviceId": await this.deviceInfoService.getDeviceId(),
       "deviceModel": await this.deviceInfoService.getDeviceModel(),
       "X-APP-VERSION": await this.deviceInfoService.getAppVersion(),
       "X-API-KEY": this.apiKey,
